cfc459f85eab556614fc6aab6faad4efc00e18f4
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2019-11-06 17:32:25 -0300

    add controller changes

--- lib/devise/jwt/extensions/warden_helper.rb
+++ lib/devise/jwt/extensions/warden_helper.rb
@@ -7,15 +7,22 @@ module Devise
             config.mappings ||= {}
             config.revocation_strategies ||= {}
             models.each do |model|
-              config.mappings.merge!(model_sym(model) => model_class(model))
-              config.revocation_strategies.merge!(
-                model_sym(model) => model_class(model).jwt_revocation_strategy
-              )
+              map_model(config, model)
             end
           end
         end
 
         private
+
+        def map_model(config, model)
+          class_value = model_class(model)
+          symbol = model_sym(model)
+          config.mappings.merge!(symbol => class_value, model => class_value)
+          config.revocation_strategies.merge!(
+            symbol => model_class(model).jwt_revocation_strategy,
+            model => class_value
+          )
+        end
 
         def model_class(model)
           model.to_s.classify.constantize
