e019b7fce774cbd01d06ebd64309691f2aaad071
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2019-11-15 10:52:05 -0300

    Fix inflections

--- lib/devise_jwt_controllers.rb
+++ lib/devise_jwt_controllers.rb
@@ -1,3 +1,6 @@
+require 'devise'
+require 'devise/jwt'
+
 require 'devise/jwt/controllers/version'
 
 require 'devise/jwt/controllers/engine'
@@ -5,4 +8,5 @@ require 'devise/jwt/controllers/railtie'
 require_relative 'devise/jwt/extensions/warden_helper'
 
 require_relative 'devise/jwt/extensions/action_dispatch'
+require_relative 'devise/jwt/extensions/active_support/inflector'
 require_relative 'devise/jwt/overrides/devise'
